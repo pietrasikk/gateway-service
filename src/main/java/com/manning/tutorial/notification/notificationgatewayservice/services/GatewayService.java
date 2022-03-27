4aa24a5f547d00e9d8b20840507919d4cbb926ad
Kacper Pietrasik <pietrasikk@gmail.com>
2022-03-27 19:46:29 +0200

    Added Sleuth, Metrics

--- src/main/java/com/manning/tutorial/notification/notificationgatewayservice/services/GatewayService.java
+++ src/main/java/com/manning/tutorial/notification/notificationgatewayservice/services/GatewayService.java
@@ -3,14 +3,18 @@ package com.manning.tutorial.notification.notificationgatewayservice.services;
 import com.manning.tutorial.notification.notificationgatewayservice.model.GatewayRequest;
 import com.manning.tutorial.notification.notificationgatewayservice.model.GatewayResponse;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RequiredArgsConstructor
 public class GatewayService {
 
     private final EmailService emailService;
     private final SmsService smsService;
+    protected final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     public GatewayResponse send(GatewayRequest gatewayRequest) {
+        logger.info("In the Notification Gateway API Class");
         switch (gatewayRequest.getNotificationMode()) {
             case "EMAIL":
                 return emailService.send(gatewayRequest);
